5f6b5ba46cbd6fd538086263bed5c0c96000dbc9
Nikolai Svakhin <svakhine@gmail.com>
2016-02-04 04:47:15 +0000

    Code tweaks to protect scope/http variables in minification?

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,10 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MyController', function MyController($scope, $http) {
+myApp.controller('MyController', ['$scope', '$http', function ($scope, $http) {
     //$scope - is like a super variable that you can use 
     //to pass things from your JavaScript to your application and template, 
     //and vice-versa.
     $http.get('js/data.json').success(function(data) {
         $scope.artists = data;
     });
-
-});
+}]);
