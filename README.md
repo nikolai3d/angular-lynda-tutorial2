837f9fdfca450c505180aeecd1f2486013df2979
Nikolai Svakhin <svakhine@gmail.com>
2016-02-05 04:11:33 +0000

    angular-route for routing

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ http://www.lynda.com/AngularJS-tutorials/Up-Running-AngularJS/154414-2.html
 
 # Simple Usage of MVC, separate file for the module & controller code
 
+# Using for for search and filtering the data list
+
+# Using AngularJS Route extra (bower install angular-route) for routing.
+
+
+
 ## Support & Documentation
 
 Visit http://docs.c9.io for support, or to learn more about using Cloud9 IDE. 
--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,9 @@
-var myApp = angular.module('myApp', []);
+var artistControllers = angular.module('artistControllers', []);
 
-myApp.controller('MyController', ['$scope', '$http', function ($scope, $http) {
-    //$scope - is like a super variable that you can use 
-    //to pass things from your JavaScript to your application and template, 
-    //and vice-versa.
-    $http.get('js/data.json').success(function(data) {
-        $scope.artists = data;
-        $scope.artistOrder = 'name'; //Set an ng-model 'name' declared in HTML
-    });
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.artists = data;
+    $scope.artistOrder = 'name';
+  });
 }]);
+
